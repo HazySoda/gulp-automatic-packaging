72e28f43a6acf17eb1500718e03484b5064cddb1
KidneyFlower <753475777@qq.com>
2017-02-18 00:59:35 +0800

    feat: Change I/O folder and the file name won't be changed now

--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,27 @@ var rev = require('gulp-rev');
 var revCollector = require('gulp-rev-collector');
 
 gulp.task('css', function () {
-    return gulp.src('src/css/*.css')
+    return gulp.src('src/**/*.css')
         .pipe(rev())
-        .pipe(gulp.dest('dist/css'))
+        .pipe(gulp.dest('dist'))
         .pipe( rev.manifest() )
         .pipe( gulp.dest( 'rev/css' ) );
 });
  
-gulp.task('scripts', function () {
-    return gulp.src('src/js/*.js')
+gulp.task('js', function () {
+    return gulp.src('src/**/*.js')
         .pipe(rev())
-        .pipe(gulp.dest('dist/js'))
+        .pipe(gulp.dest('dist'))
         .pipe( rev.manifest() )
         .pipe( gulp.dest( 'rev/js' ) );
 });
 
 gulp.task('rev', function () {
-    return gulp.src(['rev/**/*.json', 'src/aspx/*.aspx'])
-        .pipe( revCollector({
-            replaceReved: true,
-            dirReplacements: {
-                'css': '/dist/css',
-                '/js/': '/dist/js/'
-            }
-        }) )
-        .pipe( gulp.dest('dist/aspx') );
+    return gulp.src(['rev/**/*.json', 'src/**/*.aspx'])
+        .pipe( revCollector() )
+        .pipe( gulp.dest('dist') );
 });
 
 gulp.task('default', function() {
-	
+
 });
